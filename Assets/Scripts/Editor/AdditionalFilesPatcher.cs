652d0e4f58322d026dee801bcb33ff4487550443
amenonegames <amenone.games@gmail.com>
2024-01-28 19:01:41 +0900

    saishin

---
+++ Assets/Scripts/Editor/AdditionalFilesPatcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using UnityEditor;
+
+namespace DefaultNamespace
+{
+    public class AdditionalFilesPatcher : AssetPostprocessor
+    {
+        public static string OnGeneratedCSProject(string path, string content)
+        {
+            if (!path.EndsWith("Assembly-CSharp.csproj")) return content;
+       
+            var myFiles = AssetDatabase.GetAllAssetPaths().Where(file => file.EndsWith(".csv"));
+       
+            var xDoc = XDocument.Parse(content);
+            var nsMsbuild = (XNamespace)"http://schemas.microsoft.com/developer/msbuild/2003";
+            var project = xDoc.Element(nsMsbuild + "Project");
+ 
+            if (project == null) return content;
+       
+            var itemGroup = new XElement(nsMsbuild + "ItemGroup");
+            project.Add(itemGroup);
+       
+            foreach (var myFile in myFiles)
+            {
+                var include = new XAttribute("Include", myFile);
+                var item = new XElement(nsMsbuild + "AdditionalFiles", include);
+           
+                itemGroup.Add(item);
+            }
+       
+            content = xDoc.ToString();
+       
+            return content;
+        }
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Test : MonoBehaviour
+    {
+        private void Start()
+        {
+           MasterData data;
+        }
+    }
+}
